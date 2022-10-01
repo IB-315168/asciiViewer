ca3d95182a70b5046a301d33d6847e0994cbd840
IB-315168 <315168@via.dk>
2022-10-01 21:33:12 +0200

    Initial Layout + Widget creation

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import random
+
 from kivy.app import App
+from kivy.config import Config
+from kivy.properties import StringProperty
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.widget import Widget
 
+Config.set('graphics', 'resizable', False)
 
-class AsciiViewer(Widget):
+
+class AsciiViewer(RelativeLayout):
     pass
+
+
+class TextWidget(Widget):
+    random_number = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(TextWidget, self).__init__(**kwargs)
+        self.random_number = str(random.randint(1, 100))
+
+    def change_text(self):
+        self.random_number = str(random.randint(1, 100))
 
 
 class AsciiViewerApp(App):
